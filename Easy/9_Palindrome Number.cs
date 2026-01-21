51cd5a3b809f82b8a1a64f8757e9ebfe90df4ee4
HIBA <ibrahimih.hiba@gmail.com>
2026-01-21 21:51:33 +0100

    Day 7

---
+++ Easy/9_Palindrome Number.cs
@@ -0,0 +1,21 @@
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        // Les nombres négatifs ne sont pas des palindromes
+        if (x < 0)
+            return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x > 0)
+        {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
