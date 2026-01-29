4dd2c2a85b29953628f8f02e8fff42969746c596
HIBA <ibrahimih.hiba@gmail.com>
2026-01-29 17:00:49 +0100

    Day 12

--- Easy/26_Remove Duplicates Sorted Array.cs
+++ Easy/26_Remove Duplicates Sorted Array.cs
@@ -20,3 +20,29 @@ public class Solution
         return i + 1;
     }
 }
+
+// another solution usig write
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            return 0;
+        }
+
+        int write = 1; // le 1er élément est toujours unique
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[write] = nums[i];
+                write++;
+            }
+        }
+
+        return write;
+    }
+}
