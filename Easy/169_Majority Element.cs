151d2fa9401a0f2c68f77e81a5397fee8c03bdbb
HIBA <ibrahimih.hiba@gmail.com>
2026-01-30 01:31:36 +0400

    Day 12

---
+++ Easy/169_Majority Element.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        Dictionary < int,int > freq = new Dictionary< int , int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (freq.ContainsKey(nums[i]))
+            {
+                freq[nums[i]]++;
+            }
+            else
+            {
+                freq[nums[i]] = 1;
+            }
+
+            if ( //freq[nums[i]]> nums.Length/2)
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
